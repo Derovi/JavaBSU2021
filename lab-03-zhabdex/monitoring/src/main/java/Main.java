397258b1de4fa55122b79f08fe5b67bda04bfb9b
polchernikova <polchernikova@.gmail.com>
2021-10-19 09:03:55 +0300

    added table of available services

--- lab-03-zhabdex/monitoring/src/main/java/Main.java
+++ lab-03-zhabdex/monitoring/src/main/java/Main.java
@@ -1,9 +1,37 @@
-import processed_collections.*;
+import by.derovi.service_monitoring.visualizer.Table;
+import by.derovi.service_monitoring.visualizer.TerminalRenderer;
+import by.zhabdex.common.Service;
+import by.zhabdex.common.Tools;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import java.net.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args)  {
-
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TerminalRenderer renderer = TerminalRenderer.init(1);
+        URL url = new URL("http://zhabdex.ovi.by/status");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        while (true) {
+            Table table = new Table("Monitor");
+            table.addRow("Name", "Data Center", "Ping", "Available nodes", "Requests/sec", "Started time", "Current time");
+            List<Service> services = Tools.JSON.readValue(url, new TypeReference<List<Service>>() {
+            });
+            for (Service service : services) {
+                table.addRow(service.getName(),
+                        service.getDataCenter(),
+                        Long.toString(service.getAveragePing()),
+                        Integer.toString(service.getNodesCount()),
+                        Long.toString(service.getRequestsPerSecond()),
+                        service.getStartedTime().toString(),
+                        service.getCurrentTime().toString());
+            }
+            renderer.render(List.of(table));
+            Thread.sleep(1000);
+        }
     }
 }
